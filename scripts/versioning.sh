8eb07c3c70ffd68dec145b952a18fccabd1a62c0
Javier <javier.gomez@mobysoft.com>
2017-01-11 15:10:07 +0000

    Initial Version

---
+++ scripts/versioning.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+commit_date=`git show -s --format="%ct"`
+formatted_date=`date -d @${commit_date} +"%Y%m%d%H%M"`
+incremental_version=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[2]}'`
+mvn versions:set -DnewVersion=${formatted_date}-${incremental_version}-SNAPSHOT  
---
+++ src/main/java/com/gitflow/spike/project3/Service3.java
@@ -0,0 +1,27 @@
+package com.gitflow.spike.project3;
+
+
+import com.gitflow.spike.common.Util;
+import com.gitflow.spike.project1.Service;
+import com.gitflow.spike.project2.Service2;
+
+/**
+ * Created by Javier on 11/01/2017.
+ */
+public class Service3 {
+
+    Service service;
+
+    Service2 service2;
+
+    public Service3(){
+        service = new Service();
+        service2 = new Service2();
+    }
+
+    public void  execute(){
+        Util.print();
+        service.execute();
+        service2.execute();
+    }
+}
